274e7a4701f74b0b9c2cdad6ee7660bf072cf23f
Ryan Xiao <ryan.shaw.0419@google.com>
2019-02-08 00:37:22 +1100

    set up with users table

---
+++ data/create-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+   username varchar(255) primary key,
+   password varchar(255),
+   date_created timestamp NOT NULL
+);
---
+++ data/insert-test-data.sql
@@ -0,0 +1,3 @@
+insert into users values ('admin', 'admin', '2019-02-02 01:01:10');
+insert into users values ('user', 'user', '2019-02-02 01:01:10');
+insert into users values ('testuser', 'password', '2019-02-02 01:01:10');
